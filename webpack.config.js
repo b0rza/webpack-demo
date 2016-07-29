8378d6d0c778185fa513376989363cbae6e72671
Ante Borzić <aborzic@extensionengine.com>
2016-07-29 12:07:22 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const merge = require('webpack-merge');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(common, {});
+}
+
+module.exports = config;
